f7bdc04ffd6f47343c528a52f5afcc547cf356e1
Patrik <patrik-israel@hotmail.com>
2024-06-24 10:12:11 -0300

    Classe Disponibilidade criada

--- src/main/java/com/example/Projeto/Data/Disponibilidade.java
+++ src/main/java/com/example/Projeto/Data/Disponibilidade.java
@@ -1,2 +1,67 @@
-package com.example.Projeto.Data;public class Disponibilidade {
+package com.example.Projeto.Data;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+public class Disponibilidade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate data;
+
+    @Column(nullable = false)
+    private LocalTime horario;
+
+    @ManyToOne
+    @JoinColumn(name = "medico_id", nullable = false)
+    private Medico medico;
+
+    @Column(nullable = false)
+    private boolean disponivel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public LocalTime getHorario() {
+        return horario;
+    }
+
+    public void setHorario(LocalTime horario) {
+        this.horario = horario;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
 }
